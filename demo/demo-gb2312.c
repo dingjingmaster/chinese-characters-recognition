768eab914c54b66f6aee7f05ca6cc7116013978b
dingjing <dingjing@live.cn>
2022-03-29 16:13:23 +0800

    gb2312 ok

--- demo/demo-gb2312.c
+++ demo/demo-gb2312.c
@@ -2,12 +2,43 @@
 // Created by dingjing on 3/28/22.
 //
 #include <stdio.h>
+#include <string.h>
 #include <stdint.h>
+#include <stdbool.h>
+
+/**
+ * 总共 94 个区 94 个位
+ */
+bool is_gb2312 (const char* code)
+{
+    int len = strlen (code);
+    if (len != 2 && len != 1) {
+        return false;
+    }
+
+    uint8_t codeH = code[0];
+    uint8_t codeL = code[1];
+
+    uint8_t area = codeH - 0xA0;
+    uint8_t pos  = codeL - 0xA0;
+
+    if ((len == 1) && (0 == (codeL & 0x80))) {
+        printf ("ascii code: '%-3c' hex code: 0x%-2X\n", *code, *code);
+        return true;
+    }
+
+    if (area >= 1 && area <= 94 && pos >= 1 && pos <= 94) {
+        printf ("gb2312 code: '%-3s' -- area: %-2u, pos: %-2u hex code: 0x%-2X 0x%-2X\n", code, area, pos, codeH, codeL);
+        return true;
+    }
+}
+
 
 int main (int argc, char* argv[])
 {
     /**
      * https://uic.io/en/charset/show_raw/gb2312
+     * http://tools.jb51.net/table/gb2312
      * 1. '残'   十六进制: B2D0
      * 2. '怖'   十六进制: B2C0
      * 3. '惭'   十六进制: B2D1
@@ -22,32 +53,27 @@ int main (int argc, char* argv[])
      *
      * uint16_t 就足够了
      */
-    const uint16_t* arr[] = {"残", "怖", "惭",
-                             "啊", "齄",
-                             " ", "/",
-                             "0", "9",
-                             ":", "@",
-                             "A", "Z",
-                             "[", "`",
-                             "a", "z",
-                             "{", "~"};
+    const char* arr[] = {
+            "、", "∩", "〓",                              /* 第一区 */
+            "残", "怖", "惭", "庄", "丽", "君",
+            " ", "/",
+            "0", "9",
+            ":", "@",
+            "A", "Z",
+            "[", "`",
+            "a", "z",
+            "{", "~"
+    };
 
-    printf ("array: %d/%d\n\n", sizeof arr[0], sizeof arr);
+    printf ("array: %lu/%lu\n\n", sizeof arr[0], sizeof arr);
 
     int len = sizeof arr / sizeof arr[0];
     for (int i = 0; i < len; ++i) {
-        uint16_t code = *(arr[i]);
+        const char* code = arr[i];
 
-        // 小端，所以...
-        uint8_t bL = code >> 8;
-        uint8_t bH = code & 0XFF;
-
-        printf ("index: %-2d ---> gb2312 hex code: 0X%-08X "
-                "== code: %-12u "
-                "== area: 0X%-08X -- pos: 0X%-08X "
-                "==> character: %-6s\n",
-                i, code, code, bH - 0xA0, bL - 0xA0, arr[i]);
-        //printf ("debug: %-08X - %-08X\n", bH, bL);
+        if (!is_gb2312 (code)) {
+            printf ("index: %d -- code '%-3s' is not gb2312\n", i, code);
+        }
     }
 
     return 0;
